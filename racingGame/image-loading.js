68b79148967f8466077a9bfca04eca49ca7cbdad
fuckhtml <jordenblacksnk@gmail.com>
2018-08-25 21:06:45 +0300

    auto count to load good way

--- racingGame/image-loading.js
+++ racingGame/image-loading.js
@@ -2,12 +2,27 @@ var carPic = document.createElement("img");
 var wallPic = document.createElement("img");
 var roadPic = document.createElement("img");
 
-var picsToLoad = 3;
+var picsToLoad = 0; // set automatically based on imageList in loadImages();
 
 function loadImages() {
-  beginLoadImage(carPic, "player_1.png");
-  beginLoadImage(wallPic, "track_wall.png");
-  beginLoadImage(roadPic, "track_road.png");
+  var dataSet = {
+    varName: carPic, 
+    theFile: "player_1.png"
+  };
+  var imageList = [
+    { varName: carPic, 
+      theFile: "player_1.png" },
+    { varName: wallPic, 
+      theFile: "track_wall.png" },
+    { varName: roadPic, 
+      theFile: "track_road.png" }
+  ];
+
+  picsToLoad = imageList.length;
+
+  for (var i = 0; i < imageList.length; i++) {
+    beginLoadImage(imageList[i].varName, imageList[i].theFile);
+  }
 }
 
 function beginLoadImage(imgVar, fileName) {
